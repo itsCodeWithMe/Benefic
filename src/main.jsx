02d4a7c1fde15b74a90f77c5e57a9ba33bf93657
itsCodeWithMe <156082701+itsCodeWithMe@users.noreply.github.com>
2024-05-08 12:09:04 +0530

    updated in practice

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { RouterProvider,createBrowserRouter } from "react-router-dom";
 import * as Imports from "./components/Imports.jsx"
+import App from "./App.jsx";
 import Homepage from "./Pages/Homepage.jsx";
 import Aboutpage from "./Pages/Aboutpage.jsx"
 import Ourservices from "./Pages/Ourservices.jsx"
 import Uiuxdesign from "./Pages/ui-ux-designer.jsx";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 const routing=createBrowserRouter([
-    {path:"/",element:<App/>,children:[
-      {path:"/", element:<Homepage/>},
-      {path:"/contact",element:<Imports.Banner/>},
-      {path:"/about",element:<Aboutpage/>},
-      {path:"/services",element:<Ourservices/>},
-      {path:"/ui-ux-designer",element:<Uiuxdesign/>}
-    ]}
+  {path:"/",element:<App/>,children:[
+    {path:"/", element:<Homepage/>},
+    {path:"/contact",element:<Imports.Banner/>},
+    {path:"/about",element:<Aboutpage/>},
+    {path:"/services",element:<Ourservices/>},
+    {path:"/ui-ux-designer",element:<Uiuxdesign/>}
+  ]}
 ])
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={routing}/>
   </React.StrictMode>
 );
+
